1c69501392b36eee8f4a4a1378601e16f47d14d5
gijoe2day <gjuliao32@hotmail.com>
2020-08-12 22:51:26 -0500

    Updates

---
+++ precache-manifest.a832eaf3b973253ee81b99a7686fd2b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c82a69d58f98fe44f1be1548a7026240",
+    "url": "/React-City-Guide/index.html"
+  },
+  {
+    "revision": "a8679e38a359128afb62",
+    "url": "/React-City-Guide/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "28ee5dc9b3fdb3b328ac",
+    "url": "/React-City-Guide/static/css/main.799b5497.chunk.css"
+  },
+  {
+    "revision": "a8679e38a359128afb62",
+    "url": "/React-City-Guide/static/js/2.6655e57e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-City-Guide/static/js/2.6655e57e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28ee5dc9b3fdb3b328ac",
+    "url": "/React-City-Guide/static/js/main.028cf8cb.chunk.js"
+  },
+  {
+    "revision": "f007223cecea3e792876",
+    "url": "/React-City-Guide/static/js/runtime-main.8163dfde.js"
+  },
+  {
+    "revision": "f391e657f9cabe91f018ca14e340d693",
+    "url": "/React-City-Guide/static/media/logo.f391e657.svg"
+  }
+]);
